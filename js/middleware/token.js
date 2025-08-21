b60640c330994db85bc92d56e0988f796ffef681
mf <925697@163.com>
2025-08-21 15:57:56 +0800

    11

--- js/middleware/token.js
+++ js/middleware/token.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "../config.js"
+import { reqestWrapper } from "./util.js"
 
 // 中间件：验证 JWT
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) {
-        return res.status(401).json({ message: '需要提供访问令牌' });
+        return res.send(reqestWrapper("需要提供访问令牌", "fail"));
     }
     jwt.verify(token, JWT_SECRET, (err, decodedPayload) => {
         if (err) {
-            console.error('JWT 验证失败:', err.message);
-            return res.status(403).json({ message: '令牌无效或已过期' });
+            return res.send(reqestWrapper("令牌无效或已过期", "fail"));
         }
         req.user = decodedPayload;
         next();
