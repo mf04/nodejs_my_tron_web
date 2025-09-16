51b5c6ebfbf5f51af4cca02486d4a3f4b9118e7a
mf <925697@163.com>
2025-09-16 11:21:42 +0800

    11

--- js/balanceService.js
+++ js/balanceService.js
@@ -1,13 +1,9 @@
-import { balanceLogGenerate, updateUserBalance } from "./cmd_index.js"
+import { balanceLogGenerate, updateUserBalance } from "./MyMysql/CmdIndex.js"
 import { bigNumSub } from "./util.js";
 
 const balanceService = {
 
     init: async function (item, currency, fromType) {
-        // console.log(item);
-        // console.log(currency);
-        // console.log(fromType);
-        // console.log(this);
         const balanceAfter = bigNumSub(item.balance, item.amount);
         await this.userBalanceUpdate(balanceAfter, item.user_id);
         await this.balanceLogInsert([
